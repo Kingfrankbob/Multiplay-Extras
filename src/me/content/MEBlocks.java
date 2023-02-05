3ac30b79e491ef8dd5f7fe7ee287c34955368351
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2023-02-05 09:58:50 -0600

    Update MEBlocks.java

--- src/me/content/MEBlocks.java
+++ src/me/content/MEBlocks.java
@@ -1,18 +1,43 @@
 package me.content;
 
-import arc.graphics.Color;
-import mindustry.content.*;
-import mindustry.entities.bullet.BasicBulletType;
-import mindustry.gen.Sounds;
-import mindustry.world.blocks.defense.turrets.PowerTurret;
-import mindustry.world.blocks.production.AttributeCrafter;
-import mindustry.world.blocks.production.Drill;
-import mindustry.world.blocks.production.GenericCrafter;
-import mindustry.type.Category;
-import mindustry.type.LiquidStack;
-import mindustry.world.Block;
-import mindustry.world.consumers.ConsumeItemFlammable;
+import arc.graphics.*;
+import arc.math.*;
+import arc.struct.*;
+import mindustry.*;
+import mindustry.entities.*;
+import mindustry.entities.abilities.*;
+import mindustry.entities.bullet.*;
+import mindustry.entities.effect.*;
+import mindustry.entities.part.DrawPart.*;
+import mindustry.entities.part.*;
+import mindustry.entities.pattern.*;
+import mindustry.gen.*;
+import mindustry.graphics.*;
+import mindustry.type.*;
+import mindustry.type.unit.*;
+import mindustry.world.*;
+import mindustry.world.blocks.*;
+import mindustry.world.blocks.campaign.*;
+import mindustry.world.blocks.defense.*;
+import mindustry.world.blocks.defense.turrets.*;
+import mindustry.world.blocks.distribution.*;
+import mindustry.world.blocks.environment.*;
+import mindustry.world.blocks.heat.*;
+import mindustry.world.blocks.legacy.*;
+import mindustry.world.blocks.liquid.*;
+import mindustry.world.blocks.logic.*;
+import mindustry.world.blocks.payloads.*;
+import mindustry.world.blocks.power.*;
+import mindustry.world.blocks.production.*;
+import mindustry.world.blocks.sandbox.*;
+import mindustry.world.blocks.storage.*;
+import mindustry.world.blocks.units.*;
+import mindustry.world.consumers.*;
 import mindustry.world.draw.*;
+import mindustry.world.meta.*;
+
+import static mindustry.Vars.*;
+import static mindustry.type.ItemStack.*;
 
 import static mindustry.type.ItemStack.*;
 
@@ -41,9 +66,8 @@ public class MEBlocks {
 
     lightningEnhancer = new PowerTurret("enhancerL") {{
 			requirements(Category.turret, with(
-							Items.silicon, 120,
-							Items.copper, 70,
-							SWItems.compound, 95
+							Items.lead, 120,
+							Items.copper, 70
 			));
 			size = 2;
 			health = 200;
