195b4dec50fdba80699f130da9fd0b48fee488f0
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2023-02-05 09:54:20 -0600

    Add files via upload

---
+++ src/me/MEVars.java
@@ -0,0 +1,29 @@
+package me;
+
+import me.content.*;
+import me.entities.SWEntityMapping;
+
+public class MEVars {
+
+    /**This is where you initialize your content lists. But do not forget about correct order.
+     *  correct order:
+     *  ModItems.load()
+     *  ModStatusEffects.load()
+     *  ModLiquids.load()
+     *  ModBullets.load()
+     *  ModUnitTypes.load()
+     *  ModBlocks.load()
+     *  ModPlanets.load()
+     *  ModSectorPresets.load()
+     *  ModTechTree.load()
+     * */
+    public static void loadContent() {
+//         MEItems.load();
+//         MELiquids.load();
+//         MEEntityMapping.load();
+//         MEUnitTypes.load();
+        MEBlocks.load();
+//        SWPlanets.load();
+//         METechTree.load();
+    }
+}
