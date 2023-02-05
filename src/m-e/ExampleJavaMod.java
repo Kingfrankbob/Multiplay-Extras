d212c85e8c5807877f6f5f43912ae293ddaeaaae
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2023-02-05 09:07:10 -0600

    Update ExampleJavaMod.java

--- src/m-e/ExampleJavaMod.java
+++ src/m-e/ExampleJavaMod.java
@@ -1,4 +1,4 @@
-package example;
+package me;
 
 import arc.*;
 import arc.util.*;
@@ -7,30 +7,31 @@ import mindustry.content.*;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.mod.Mod;
 import mindustry.ui.dialogs.*;
 
-public class ExampleJavaMod extends Mod{
+public class MEModLoad extends Mod{
 
-    public ExampleJavaMod(){
-        Log.info("Loaded ExampleJavaMod constructor.");
+    public MEModLoad(){
+//         Log.info("Loaded ExampleJavaMod constructor.");
 
-        //listen for game load event
-        Events.on(ClientLoadEvent.class, e -> {
-            //show dialog upon startup
-            Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
-            });
-        });
+//         //listen for game load event
+//         Events.on(ClientLoadEvent.class, e -> {
+//             //show dialog upon startup
+//             Time.runTask(10f, () -> {
+//                 BaseDialog dialog = new BaseDialog("frog");
+//                 dialog.cont.add("behold").row();
+//                 //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+//                 dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+//                 dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+//                 dialog.show();
+//             });
+//         });
     }
 
     @Override
     public void loadContent(){
-        Log.info("Loading some example content.");
+        MEVars.loadContent();
     }
 
 }
