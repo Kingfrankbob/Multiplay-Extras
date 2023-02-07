bbe6ddd285a9439e1565fb897a094ed08f7ae64b
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2023-02-06 19:06:42 -0600

    Update MEBlocks.java

--- src/me/content/MEBlocks.java
+++ src/me/content/MEBlocks.java
@@ -132,7 +132,7 @@ public class MEBlocks {
 		}};
 		
 		
-	    arcaicReleaser = new PowerTurret("araicRelease"){{
+	    arcaicReleaser = new PowerTurret("arcaicRelease"){{
             requirements(Category.turret, with(Items.copper, 75, Items.lead, 25, Items.coal, 50, Items.sand, 50));
             shootType = new LightningBulletType(){{
                 damage = 20;
