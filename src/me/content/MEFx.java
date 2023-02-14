6869b3b085ecbea8ed6cb9ad83a2a6a95509424e
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2023-02-14 06:20:43 -0600

    Update MEFx.java

--- src/me/content/MEFx.java
+++ src/me/content/MEFx.java
@@ -118,8 +118,8 @@ public class MEFx
   
   firstShadow = new Effect(100f, e -> {
 //     Drawf.shadow(e.x, e.y, 10f, 4f);
-    Draw.color(0, 0, 0, 0.4f * 3f);
-        Draw.rect("circle-shadow", e.x, e.y, 100f, 10f);
+    Draw.color(0, 0, 0, 0.4f * (1f * e.fout()));
+        Draw.rect("circle-shadow", e.x, e.y, 600f, 600f);
         Draw.color();
     Log.info("Shadow Effect is running!!!");
     
