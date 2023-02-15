4dc0b77f9540ca217a564b12fb6e812a28568fc4
Kingfrankbob <67441018+Kingfrankbob@users.noreply.github.com>
2023-02-14 19:03:48 -0600

    Update MEFx.java

--- src/me/content/MEFx.java
+++ src/me/content/MEFx.java
@@ -131,7 +131,7 @@ public class MEFx
     Draw.z(99);
     Draw.color(0, 0, 0, 0.4f * 1f);
     Draw.rect("circle-shadow", e.x, e.y, 600f, 600f);
-    Draw.rect("me-satelliteCrash", e.x, e.y, (e.fout() * 200f) + 50f, (e.fout() * 400f) + 100f, (e.fin() * 360));
+    Draw.rect("me-satelliteCrash", e.x, e.y, 200f + 50f, 400f 100f, (e.fin() * 360));
     Draw.color();    
   }),
 
